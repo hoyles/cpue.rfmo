49d22df1fd29c93ee54be9d21d1dd0cc76fb33e8
Ricardo Oliveros-Ramos <ricardo.oliveros@gmail.com>
2019-02-14 17:03:21 -0800

    NAMESPACE generated by Roxygen2 (aaa-.R)

---
+++ R/aaa-.R
@@ -0,0 +1,39 @@
+#' @exportPattern ^[[:alpha:]]+
+NULL
+#' @import stats graphics
+NULL
+#' @importFrom beanplot beanplot
+NULL
+#' @importFrom boot logit inv.logit
+NULL
+#' @importFrom cluster clara
+NULL
+#' @importFrom data.table data.table as.data.table setkeyv
+NULL
+#' @importFrom dplyr case_when mutate
+NULL
+#' @importFrom grDevices dev.cur dev.new dev.set graphics.off heat.colors savePlot windows
+NULL
+#' @importFrom lubridate week ymd
+NULL
+#' @importFrom lunar lunar.illumination
+NULL
+#' @importFrom magrittr %>%
+NULL
+#' @importFrom maps map
+NULL
+#' @importFrom nFactors nScree plotnScree
+NULL
+#' @importFrom readr parse_character parse_date parse_integer
+NULL
+#' @importFrom splines ns
+NULL
+#' @importFrom stringr str_c
+NULL
+#' @importFrom utils capture.output read.csv relist write.csv object.size
+NULL
+
+
+
+
+
